debd0ae1af422f51540a5d8774e95c1853220ab9
martha58 <marthafridayimoh@gmail.com>
2025-02-26 15:58:26 +0100

    "Extractind data from nosql database"

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import duckdb
 import pandas as pd
 import pymongo
 import certifi
+import os
 from pymongo import MongoClient
 
 def extract_data_api():
@@ -30,7 +31,7 @@ def extract_data_api():
             print("Error:", response.status_code)
 
     try:
-        connection = "mongodb+srv://local_business_user:password@cluster0.0j2dc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        connection = "mongodb+srv://local_business_user:local_business@cluster0.0j2dc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
         client = pymongo.MongoClient(connection, tlscaFile=certifi.where()) 
         print("Connected to MongoDB")
     except Exception as e:
@@ -46,6 +47,36 @@ def extract_data_api():
     return data
 extract_data_api()
 
+def extract_data_from_nosql_database():
+    try:
+        connection = "mongodb+srv://local_business_user:local_business@cluster0.0j2dc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        client = pymongo.MongoClient(connection, tlscaFile=certifi.where()) 
+        print("Connected to MongoDB")
+    except Exception as e:
+        print(f"Erro: {e}")
 
+    # Extract specific data from the collection
+    db = client['local_business_db']
+    collection = db['local_business_collection']
+
+    extracted_data = []
+
+    for document in collection.find({}, {'id': False}):
+        if 'data' in document and isinstance(document['data'], list):
+            for data in document['data']:
+                extracted_data.append({
+                    'name': data.get('name', None),
+                    'address': data.get('full_address', None),
+                    'city': data.get('city', None),
+                    'phone_number': data.get('phone_number', None),
+                    'review_count': data.get('review_count', None),
+                    'rating': data.get('rating', None),
+                    'website': data.get('website', None),
+                    'description': data.get('description', None),
+                    'types': data.get('types', [])
+                })
+    print(f"Extracted data {len(extracted_data)}")
+    return extracted_data
+extract_data_from_nosql_database()
 
     
